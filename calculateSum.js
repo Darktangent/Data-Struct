486f86ecbdd64d1741dcd25aba721ce5e2aa602f
Rohan <c1ph3r9@gmail.com>
2018-10-31 02:01:22 -0500

    fixing calculate sum function

--- calculateSum.js
+++ calculateSum.js
@@ -6,8 +6,8 @@ function calculateNum(input){
     if (input===0 || input===1){
         return input
     }
-    var j=1
-    for (var i=0;i<input;i++){
+    var j=2
+    for (var i=1;i<=input;i++){
         sum=i+j
         j++
     }
